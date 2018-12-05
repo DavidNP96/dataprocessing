83e335bfe7bc688514341aae29250b3ec49c4ba7
David Pantophlet <dnpantophlet@gmail.com>
2018-12-05 18:20:25 +0100

    final

--- Homework/week_5/scatter.js
+++ Homework/week_5/scatter.js
@@ -19,8 +19,8 @@ window.onload = function() {
   d3.select("body").append("p").text("student number: 12466638");
 
 
-  var consConf = "http://stats.oecd.org/SDMX-JSON/data/HH_DASH/FRA+DEU+KOR+NLD+PRT+GBR.COCONF.A/all?startTime=2007&endTime=2015"
-  var womenInScience = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
+  var consConf = "https://stats.oecd.org/SDMX-JSON/data/HH_DASH/FRA+DEU+KOR+NLD+PRT+GBR.COCONF.A/all?startTime=2007&endTime=2015"
+  var womenInScience = "https://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
 
   var requests = [d3.json(womenInScience), d3.json(consConf)];
 
